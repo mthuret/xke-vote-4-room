3d1f6069d93de8953d813087313678fdc52210f2
Marie-Laure Thuret <mthuret@xebia.fr>
2016-05-28 13:55:32 +0200

    improvement of react storybook usage

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,6 +2,8 @@ import { configure } from '@kadira/storybook';
 
 function loadStories() {
   require('../src/components/stories/slot');
+  require('../src/components/stories/slots');
+  require('../src/components/stories/votes');
 }
 
 configure(loadStories, module);
--- client/src/components/VotingResultsView/TalkProgressBar.js
+++ client/src/components/VotingResultsView/TalkProgressBar.js
@@ -1,44 +1,46 @@
-import React from 'react';
-import LinearProgress from 'material-ui/lib/linear-progress';
-import {getFondationStyle} from '../fondations.js';
-import Avatar from 'material-ui/lib/avatar';
-
-import styles from 'material-ui/lib/styles';
+import React from "react";
+import LinearProgress from "material-ui/lib/linear-progress";
+import {getFondationStyle} from "../fondations.js";
+import Avatar from "material-ui/lib/avatar";
+import styles from "material-ui/lib/styles";
 
 const colors = styles.Colors;
 
 const style = {
-  margin: 10
+
+  globalcontainer: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: 10
+  },
+  information: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    marginLeft: 10
+  }
+
 };
 
-export const TalkProgressBar = ({ text, attendees, fondation, room }) => (
-  <div className='container-fluid'>
-    <div className='row ' style={style}>
-      <div className='col-md-1'>
-        <Avatar
-          color={colors.white}
-          backgroundColor={getFondationStyle(fondation).color}
-          style={{fontSize: '13px', top: '-px'}}
-        >
-          {attendees}
-        </Avatar>
+export const TalkProgressBar = ({text, attendees, fondation, room}) => (
+  <div style={style.globalcontainer}>
+    <Avatar
+      color={colors.white}
+      backgroundColor={getFondationStyle(fondation).color}
+      style={{fontSize: '13px', top: '-px'}}>
+      {attendees}
+    </Avatar>
+    <div style={style.information}>
+      <div>
+        {text}
+        <div style={{fontWeight: 'bold'}}>Salle : {room}</div>
       </div>
-      <div className='col-md-11'>
-        <div className='row'>
-          <div className='col-md-8'>
-            {text}
-            <div style={{fontWeight: 'bold'}}>Salle : {room}</div>
-          </div>
-          <div className='col-md-12'>
-            <LinearProgress mode='determinate'
-                            value={attendees}
-                            max={70}
-                            style={{height: '5px'}}
-                            color={getFondationStyle(fondation).color}
-                            />
-          </div>
-        </div>
-      </div>
+      <LinearProgress mode='determinate'
+                      value={attendees}
+                      max={70}
+                      style={{height: '5px'}}
+                      color={getFondationStyle(fondation).color}
+      />
     </div>
   </div>
 );
